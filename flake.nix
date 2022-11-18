1e626ad05ee3e05862b3b779aeef91d2e0fdd220
Sofi <sofi+git@mailbox.org>
2022-11-18 17:36:01 +0100

    refactor(flake): Proper multiarch support

--- flake.nix
+++ flake.nix
@@ -3,52 +3,81 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    # Utils
     deploy-rs.url = "github:serokell/deploy-rs";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    # Packages
     nix-minecraft.url = "github:imsofi/nix-minecraft/develop";
     nix-minecraft.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, deploy-rs, sops-nix, pre-commit-hooks, nix-minecraft, ... }@inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , deploy-rs
+    , pre-commit-hooks
+    , sops-nix
+    , nix-minecraft
+    , ...
+    } @ inputs:
     let
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      foldEachSystem = systems: f:
+        builtins.foldl' nixpkgs.lib.recursiveUpdate { } (nixpkgs.lib.forEach systems f);
+
       commonModule = {
-        # Helps error message know where this module is defined, avoiding `<unknown-file>` in errors.
         _file = ./flake.nix;
         imports = [
           sops-nix.nixosModules.sops
         ];
         config = {
           nixpkgs.config.allowUnfree = true;
-          nixpkgs.overlays = nixpkgs.lib.attrValues self.overlays ++ [
-            nix-minecraft.overlays.default
-          ];
-          # Follow this git repo's hashes and last modified date for version suffixes, instead of
-          # using upstream nixpkgs.
-          system.nixos.versionSuffix = nixpkgs.lib.mkForce ".${
-            nixpkgs.lib.substring 0 8 (self.lastModifiedDate or self.lastModified)}.${
-            self.shortRev or "dirty"}";
-          # WORKAROUND: Nix struggles with revisions in dirty repos -> https://github.com/NixOS/nix/pull/5385
+          nixpkgs.overlays =
+            nixpkgs.lib.attrValues self.overlays
+            ++ [
+              nix-minecraft.overlays.default
+            ];
+          # Use our hashes and modified dates for version
+          # suffixes, instead of upstream.
+          system.nixos.versionSuffix =
+            nixpkgs.lib.mkForce ".${
+            nixpkgs.lib.substring 0 8 (self.lastModifiedDate or self.lastModified)
+          }.${
+            self.shortRev or "dirty"
+          }";
+          # Nix struggles with revisions in dirty repos
+          # See: https://github.com/NixOS/nix/pull/5385
           system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
         };
       };
     in
+    foldEachSystem systems (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system}.pkgs;
+    in
     {
-      overlays = {
-        #  tt-rss-plugin-auth-ldap = import ./overlays/tt-rss-plugin-auth-ldap.nix;
-        tt-rss-plugin-fever = (final: prev: {
-          tt-rss-plugin-fever = final.callPackage ./pkgs/tt-rss-plugin-fever.nix { };
-        });
-      };
+      # --- Public ---
 
       nixosModules = {
         minecraft-server = import ./modules/minecraft-server.nix;
       };
 
-      packages."x86_64-linux" = let pkgs = nixpkgs.legacyPackages."x86_64-linux"; in {
+      packages.${system} = {
         tt-rss-plugin-fever = pkgs.callPackage ./pkgs/tt-rss-plugin-fever.nix { };
       };
+
+      overlays = {
+        tt-rss-plugin-fever = final: prev: {
+          tt-rss-plugin-fever = final.callPackage ./pkgs/tt-rss-plugin-fever.nix { };
+        };
+      };
+
+      # --- Systems ---
 
       nixosConfigurations = {
         nona = nixpkgs.lib.nixosSystem rec {
@@ -67,54 +96,61 @@
 
         profiles.system = {
           user = "root";
-          path = deploy-rs.lib.x86_64-linux.activate.nixos
-            self.nixosConfigurations.nona;
+          path =
+            deploy-rs.lib.x86_64-linux.activate.nixos
+              self.nixosConfigurations.nona;
         };
       };
 
-      images.x86_64-linux = {
+      # --- System images ---
+
+      images."x86_64-linux" = {
         qemu = import "${nixpkgs}/nixos/lib/make-disk-image.nix" {
           system = "x86_64-linux";
-          config = (nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-              ./common
-              ./common/hardware/qemu.nix
-            ];
-          }).config;
+          config =
+            (nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              modules = [
+                "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+                ./common
+                ./common/hardware/qemu.nix
+              ];
+            }).config;
           format = "qcow2";
           partitionTableType = "legacy";
         };
       };
 
-      devShells.x86_64-linux.default =
-        let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        in (pkgs.mkShell {
-          inherit (self.checks.x86_64-linux.pre-commit-check) shellHook;
-          nativeBuildInputs = with pkgs; [
-            # Basic packages
-            nixUnstable
-            pkgs.deploy-rs
-            # sops-nix related
-            age
-            ssh-to-age
-            sops
-          ];
-        });
+      # --- Development shell ---
 
-      checks.x86_64-linux =
+      devShells.${system}.default = pkgs.mkShell {
+        inherit (self.checks.x86_64-linux.pre-commit-check) shellHook;
+        nativeBuildInputs = with pkgs; [
+          # Basic Packages
+          nixUnstable
+          pkgs.deploy-rs
+          # Secret Encryption
+          age
+          ssh-to-age
+          sops
+          # Helpers and formatters
+          alejandra
+        ];
+      };
+
+      # --- Tests ---
+
+      checks.${system} =
         let
-          # TODO: Redo this to allow for multi architecture checks.
-          deploy-checks = deploy-rs.lib.x86_64-linux.deployChecks self.deploy;
+          deploy-checks = deploy-rs.lib.${system}.deployChecks self.deploy;
         in
         {
-          deploy-schema = deploy-checks.schema;
           deploy-activate = deploy-checks.activate;
+          deploy-schema = deploy-checks.schema;
 
-          pre-commit-check = pre-commit-hooks.lib.x86_64-linux.run {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
-            hooks = with nixpkgs.legacyPackages.x86_64-linux; {
+            hooks = {
               nixpkgs-fmt.enable = true;
               editorconfig-checker = {
                 enable = true;
@@ -132,5 +168,5 @@
             };
           };
         };
-    };
+    });
 }
