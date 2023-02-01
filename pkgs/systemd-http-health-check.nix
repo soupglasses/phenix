6917343b7e217cbc018bf22c340d30800181500f
Sofi <sofi+git@mailbox.org>
2023-02-01 16:15:14 +0100

    systemd-http-health-check: update to 2022-02-01

--- pkgs/systemd-http-health-check.nix
+++ pkgs/systemd-http-health-check.nix
@@ -6,13 +6,13 @@
 }:
 crystal.buildCrystalPackage rec {
   pname = "systemd-http-health-check";
-  version = "0.2.0";
+  version = "2022-02-01";
 
   src = fetchFromGitHub {
-    owner = "jhass";
+    owner = "imsofi";
     repo = "systemd_http_health_check";
-    rev = "v${version}";
-    hash = "sha256-c2PzuqFQqevX14cCpxvTJfd0eZTcMA6ch6ieoR2DC0g=";
+    rev = "4eb910dfc5cf4e87f6efb064f1d66c0d10550f95";
+    hash = "sha256-MqbLQhWj+b04oDTZZoCYn1zwrLSdsrLB6wSF3Q7w4Ao=";
   };
 
   format = "shards";
