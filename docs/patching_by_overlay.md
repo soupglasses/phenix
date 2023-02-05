55827538e515914565c7534a321be12ac4c82f5a
Sofi <sofi+git@mailbox.org>
2023-02-05 23:19:42 +0100

    jellyfin-hardened: init at 10.8.8

---
+++ docs/patching_by_overlay.md
@@ -0,0 +1,25 @@
+# Patching NixOS Modules by Overlay
+
+I will use the nixos module `minecraft-server.nix` for my example for how to write an overlay.
+
+First step is to create the actual module with your modification.
+
+```nix
+# ./modules/example-module.nix
+{config, lib, pkgs, ... }:
+{
+  ...
+}
+```
+
+If you are modifying a module inside of `nixpkgs/nixos`, you will need to disable that module inside of your modification. You can use https://search.nixos.org/options to find the location of your service file.
+
+Here is an example for how to disable `services.minecraft-server`. I would recommend to put this as close to the top of your module to make it easier to see.
+
+```nix
+{config, lib, pkgs, ... }:
+{
+  imports = [ ./modules/minecraft-server.nix ];
+  disabledModules = [ "services/games/minecraft-server.nix" ];
+}
+```
---
+++ pkgs/jellyfin-hardened/server.nix
@@ -0,0 +1,69 @@
+{
+  lib,
+  fetchFromGitHub,
+  nixosTests,
+  dotnetCorePackages,
+  buildDotnetModule,
+  ffmpeg,
+  fontconfig,
+  freetype,
+  jellyfin-hardened-web,
+  sqlite,
+}:
+buildDotnetModule rec {
+  pname = "jellyfin";
+  version = "10.8.8"; # ensure that jellyfin-web has matching version
+
+  src = fetchFromGitHub {
+    owner = "GermanCoding";
+    repo = "jellyfin";
+    rev = "4c06968a00c9ee339765cabd3918621691e13212";
+    hash = "sha256-6vpXJsfUgbn7j9wK3FS/pBw/7uRzkOIu83jo8VBwEE8=";
+  };
+
+  patches = [
+    # when building some warnings are reported as error and fail the build.
+    ./disable-warnings.patch
+    ./extra-disable.patch
+  ];
+
+  propagatedBuildInputs = [
+    sqlite
+  ];
+
+  projectFile = "Jellyfin.Server/Jellyfin.Server.csproj";
+  executables = ["jellyfin"];
+
+  nugetDeps = ./nuget-deps.nix;
+  packNupkg = false;
+
+  runtimeDeps = [
+    ffmpeg
+    fontconfig
+    freetype
+  ];
+  dotnet-sdk = dotnetCorePackages.sdk_6_0;
+  dotnet-runtime = dotnetCorePackages.aspnetcore_6_0;
+  dotnetBuildFlags = ["--no-self-contained"];
+
+  preInstall = ''
+    makeWrapperArgs+=(
+      --add-flags "--ffmpeg ${ffmpeg}/bin/ffmpeg"
+      --add-flags "--webdir ${jellyfin-hardened-web}/share/jellyfin-web"
+    )
+  '';
+
+  passthru.tests = {
+    smoke-test = nixosTests.jellyfin;
+  };
+
+  passthru.updateScript = ./update.sh;
+
+  meta = with lib; {
+    description = "The Free Software Media System";
+    homepage = "https://jellyfin.org/";
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [imsofi];
+    platforms = dotnet-runtime.meta.platforms;
+  };
+}
