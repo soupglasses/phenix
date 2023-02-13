d82eb7a39bd748055e0ec5c5104be86a985c3a42
Sofi <sofi+git@mailbox.org>
2023-02-13 15:24:33 +0100

    feat: add garnix as trusted substituter

--- common/default.nix
+++ common/default.nix
@@ -72,8 +72,11 @@
       warn-dirty = false;
 
       builders-use-substitutes = true;
-      substituters = ["https://nix-community.cachix.org"];
-      trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+      substituters = ["https://nix-community.cachix.org" "https://cache.garnix.io"];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
     };
   };
 
