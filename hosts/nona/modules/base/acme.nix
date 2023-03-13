e5777928f14ee7360293541b3098eeb08412d768
Sofi <sofi+git@mailbox.org>
2023-03-13 15:41:03 +0100

    feat: use deSEC for acme and remove cloudflare

--- hosts/nona/modules/base/acme.nix
+++ hosts/nona/modules/base/acme.nix
@@ -1,16 +1,22 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   security.acme.acceptTerms = true;
 
   security.acme.defaults = {
     email = "sofi+acme@mailbox.org";
-    dnsProvider = "cloudflare";
-    dnsResolver = "1.1.1.1:53";
-    credentialsFile = config.sops.secrets."acme/cloudflare_dns_api_key".path;
+    dnsProvider = "desec";
+    dnsResolver = "9.9.9.9:53";
+    credentialsFile = pkgs.writeText "credentials" ''
+      DESEC_TOKEN_FILE=${config.sops.secrets."acme/desec_token".path}
+    '';
   };
 
   security.dhparams.enable = true;
 
-  sops.secrets."acme/cloudflare_dns_api_key" = {
+  sops.secrets."acme/desec_token" = {
     owner = config.users.users.acme.name;
     sopsFile = ../../secrets/acme.yaml;
   };
