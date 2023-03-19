28aa89963d2b63279de5e8e74a1dd057bbb1fa96
Sofi <sofi+git@mailbox.org>
2023-03-19 01:08:50 +0100

    feat: move dns secrets to top-level

--- nixos/hosts/nona/modules/base/acme.nix
+++ nixos/hosts/nona/modules/base/acme.nix
@@ -10,14 +10,14 @@
     dnsProvider = "desec";
     dnsResolver = "9.9.9.9:53";
     credentialsFile = pkgs.writeText "credentials" ''
-      DESEC_TOKEN_FILE=${config.sops.secrets."acme/desec_token".path}
+      DESEC_TOKEN_FILE=${config.sops.secrets.desec_token.path}
     '';
   };
 
   security.dhparams.enable = true;
 
-  sops.secrets."acme/desec_token" = {
+  sops.secrets.desec_token = {
     owner = config.users.users.acme.name;
-    sopsFile = ../../secrets/acme.yaml;
+    sopsFile = ../../../../../secrets/dns.yaml;
   };
 }
