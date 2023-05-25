65c0608c420dce4e215fde680622eb954fc9b720
Sofi <sofi+git@mailbox.org>
2023-05-25 13:30:41 +0200

    flake: refactor overlays into independent files

--- flake.nix
+++ flake.nix
@@ -109,21 +109,8 @@
           final.lib.recurseIntoAttrs
           (import ./nixos/packages/top-level/all-packages.nix {inherit (final) pkgs;});
       };
-      prometheus-systemd-exporter = _final: prev: {
-        prometheus-systemd-exporter = prev.prometheus-systemd-exporter.overrideAttrs (_p: {
-          patches = [
-            # https://github.com/prometheus-community/systemd_exporter/pull/74
-            (prev.fetchpatch {
-              url = "https://github.com/prometheus-community/systemd_exporter/commit/0afc9bee009740825239df1e6ffa1713a57a5692.patch";
-              sha256 = "sha256-ClrV9ZOlRruYXaeQwhWc9h88LP3Rm33Jf/dvxbqRS2I=";
-            })
-            (prev.fetchpatch {
-              url = "https://github.com/prometheus-community/systemd_exporter/commit/47d7e92ec34303a8da471fd1c26106f606e5a150.patch";
-              sha256 = "sha256-Ox9IE8LeYBflitelyZr4Ih1zSt9ggjnogj6k0qI2kx4=";
-            })
-          ];
-        });
-      };
+      prometheus-systemd-exporter = import ./nixos/overlays/prometheus-systemd-exporter.nix;
+      tt-rss-plugin-auth-ldap = import ./nixos/overlays/tt-rss-plugin-auth-ldap.nix;
     };
 
     # -- Formatter --
