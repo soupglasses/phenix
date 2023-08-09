a4e7c032c91b9365ba11d7aeea374d7bf4e1e79b
Sofi <sofi+git@mailbox.org>
2023-08-10 01:08:35 +0200

    default.nix: alias packages to top-level

--- default.nix
+++ default.nix
@@ -1,18 +1,11 @@
-{system ? builtins.currentSystem}:
-(
-  import
-  (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
-  )
-  {
-    src = ./.;
-    inherit system;
-  }
-)
-.defaultNix
+{...}: let
+  flake-lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  flake-compat = fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${flake-lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = flake-lock.nodes.flake-compat.locked.narHash;
+  };
+  self = import flake-compat {src = ./.;};
+  packages = self.defaultNix.outputs.packages.${builtins.currentSystem};
+in
+  packages
+  // self.defaultNix
